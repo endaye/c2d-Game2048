7f4c4d49dcb0ebeaea87106b073691b6496ac3c0
Yuancheng Zhang <old.far.hot@gmail.com>
2016-06-27 16:50:51 -0500

    added 全局变量头文件

---
+++ Classes/gamedefine.hpp
@@ -0,0 +1,15 @@
+//
+//  gamedefine.hpp
+//  Game2048
+//
+//  Created by Yuancheng Zhang on 6/27/16.
+//
+// 定义一些基本数据的定义，来实现全局描述
+
+#ifndef gamedefine_hpp
+#define gamedefine_hpp
+
+#define GAME_SCREEN_WIDTH  320
+#define GAME_SCREEN_HEIGHT 480
+
+#endif /* gamedefine_hpp */
